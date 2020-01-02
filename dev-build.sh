e912b193406b394bee84d642c193facc8cf09aa0
RonnyDo <ronnydobra at arcor de>
2020-01-02 21:43:09 +0100

    fix minor config issues

--- dev-build.sh
+++ dev-build.sh
@@ -3,7 +3,9 @@ meson build
 cd build
 meson configure -Dprefix=/usr
 ninja
+
 sudo cp ../data/com.github.ronnydo.colorpicker.gschema.xml /usr/share/glib-2.0/schemas/
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
-./com.github.ronnydo.colorpicker
+
+G_MESSAGES_DEBUG=all ./com.github.ronnydo.colorpicker
 cd ..
